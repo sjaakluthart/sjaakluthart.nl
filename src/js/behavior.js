295135d24603c0b0f280f4d379d8bb66fc00e089
Sjaak Luthart <mail@sjaakluthart.nl>
2015-12-24 21:15:38 +0100

    moved files to src

---
+++ src/js/behavior.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	var audio = new Audio('audio/picardsong.mp3');
+
+	// Cache the Window object
+	$window = $(window);
+
+	// disable parallax effect for touch
+	if ($('html').hasClass('no-touch')){
+
+		$('.header').addClass('scroll');
+		$('.scroll').each(function(){
+	    	var $bgobj = $(this); // assigning the object
+	                    
+	      	$(window).scroll(function() {
+	                    
+				// Scroll the background at var speed
+				// the yPos is a negative value because we're scrolling it UP!								
+				var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+				
+				// Put together our final background position
+				var coords = '50% '+ yPos + 'px';
+
+				// Move the background
+				$bgobj.css({ backgroundPosition: coords });
+			
+			}); // window scroll Ends
+
+	 	});	
+	} 
+  
+  	// enable hover styles for touch
+	if ($('html').hasClass('touch')){
+
+		$('body > section > figure').click(function() {
+  			$(this).children('div').toggleClass('active')
+		});
+
+		$('body > section > figure div').click(function() {
+  			$(this).toggleClass('active')
+		});
+
+	}
+
+	var easter_egg = new Konami(function() { 
+		$('img.doge').toggleClass('active');
+		$('body').toggleClass('doge');
+		audio.play();
+	});
+
+});
