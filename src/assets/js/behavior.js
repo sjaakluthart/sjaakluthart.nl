d731c21b08ed58ca17735207cc8e5e3712c441f6
Sjaak Luthart <mail@sjaakluthart.nl>
2015-12-29 19:32:41 +0100

    removed old code

--- src/assets/js/behavior.js
+++ src/assets/js/behavior.js
@@ -1,39 +1,10 @@
 $(document).ready(function(){
 
     var audio,
-        $window,
-        html,
-        parallax;
+        html;
 
 	audio = new Audio('assets/audio/picardsong.mp3');
     html = $('html');
-	$window = $(window);
-
-	// disable parallax effect for touch
-	if (html.hasClass('no-touch')){
-
-		parallax = function($scroll){
-			$(window).scroll(function() {
-
-				// Scroll the background at var speed
-				// the yPos is a negative value because we're scrolling it UP!
-				var yPos = -($window.scrollTop() / $scroll.data('speed'));
-
-				// Put together our final background position
-				var coords = '50% '+ yPos + 'px';
-
-				// Move the background
-				$scroll.css({ backgroundPosition: coords });
-
-			});
-		};
-
-		$('.scroll').each(function(){
-            parallax($(this));
-	 	});
-
-        parallax($('header'));
-	} 
   
   	// enable hover styles for touch
 	if (html.hasClass('touch')){
