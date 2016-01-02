8780fc94dcebbc65bfc0d19fcbc328a82c14c2fd
Sjaak Luthart <mail@sjaakluthart.nl>
2016-01-02 11:42:46 +0100

    removed unused code

--- src/assets/js/behavior.js
+++ src/assets/js/behavior.js
@@ -1,24 +1,7 @@
 $(document).ready(function(){
 
-    var html;
-
-    html = $('html');
-  
-  	// enable hover styles for touch
-	if (html.hasClass('touch')){
-
-		$('body > section > figure').click(function() {
-  			$(this).children('div').toggleClass('active')
-		});
-
-		$('body > section > figure div').click(function() {
-  			$(this).toggleClass('active')
-		});
-
-	}
-
-	new Konami(function() {
-		$('.doge').toggleClass('active');
-	});
+    new Konami(function() {
+        $('.doge').toggleClass('active');
+    });
 
 });
