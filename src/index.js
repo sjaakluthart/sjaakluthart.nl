47a4a700d93ab776803908d28ee1aca007916bcb
Sjaak Luthart <mail@sjaakluthart.nl>
2017-04-08 12:41:09 +0200

    Removed boilerplate files.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+/* eslint react/jsx-filename-extension: [0] */
+/* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import './css/meyer-reset.css';
+import './css/index.css';
 
 ReactDOM.render(
-  <App />,
+  <p>hi</p>,
   document.getElementById('root')
 );
